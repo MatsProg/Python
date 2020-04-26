67e281f07eb6717a24710861c74207b7ea75fe78
MatsProg <pawelgixer@gmail.com>
2020-04-26 23:08:34 +0200

    up

---
+++ F1+ACC/OpenGL.py
@@ -0,0 +1,3 @@
+import OpenGL
+OpenGL.ERROR_LOGGING = False # if errors: OpenGL.FULL_LOGGING = True
+from OpenGL.GL import *
--- F1+ACC/testcode.py
+++ F1+ACC/testcode.py
@@ -1,30 +1,12 @@
-import glfw
-import self as self
+from START import *
+import winsound
 
-
-class glfwClass:
-    def __init__(self, width: int, height: int, title: str):
-        if not glfw.init():
-            raise Exception('nie dziala')
-
-        self._window = glfw.create_window(width, height, title, None, None)
-
-        if not self._window:
-            glfw.terminate()
-            raise Exception('window nie dziala')
-
-
-        glfw.set_window_pos(self._window, 400, 200)
-
-        glfw.make_context_current(self._window)
-
-    def window_loop_main(self):
-        while not glfw.window_should_close(self._window):
-            glfw.poll_events()
-            glfw.swap_buffers(self._window)
-
-        glfw.terminate()
-
-if __name__ == "__main__":
-    window = glfwClass(800, 600, 'Overlay')
-    window.window_loop_main()
+class overlay():
+	def __init__(self):
+		self.ex = Example()
+		self.ex.bits()
+		self.RPM = ex.RPM
+		winsound.PlaySound("C:\Program Files (x86)\FreePIE\pylib\alarm.wav", winsound.SND_FILENAME)
+	def RPM1(self):
+		if self.RPM > self.RPMsound:
+			winsound.PlaySound("C:\Program Files (x86)\FreePIE\pylib\alarm.wav", winsound.SND_FILENAME)
